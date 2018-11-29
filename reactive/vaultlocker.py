627ac2f4db74f361a63decc52b26789ae7a665a1
Cory Johns <johnsca@gmail.com>
2018-11-28 21:26:43 -0500

    Add support for creating persistent loop devices
    Add helper to manage persistent loop device for backing VaultLocker.
    Also refactor error handling.

--- reactive/vaultlocker.py
+++ reactive/vaultlocker.py
@@ -1,15 +1,23 @@
+import shutil
+
 from charms.reactive import when_all, when_not, set_flag, clear_flag
 from charmhelpers.core import hookenv
 
-from charms.layer import vault_kv
-from charms.layer import vaultlocker
+from charms import layer
 
 
 @when_all('apt.installed.vaultlocker',
           'layer.vault-kv.ready',
           'layer.vault-kv.config.changed')
 def configure_vaultlocker():
-    vaultlocker.write_vaultlocker_conf(vault_kv.get_vault_config())
+    # write VaultLocker config file
+    layer.vaultlocker.write_vaultlocker_conf(layer.vault_kv.get_vault_config())
+    # create location for loop device service envs
+    layer.vaultlocker.LOOP_ENVS.mkdir(parents=True, exist_ok=True)
+    # create loop device service template
+    shutil.copyfile('templates/vaultlocker-loop@.service',
+                    '/etc/systemd/system/vaultlocker-loop@.service')
+    # mark as complete
     set_flag('layer.vaultlocker.configured')
     clear_flag('layer.vault-kv.config.changed')
 
@@ -21,5 +29,5 @@ def auto_encrypt():
     for storage_name, storage_metadata in metadata.get('storage', {}).items():
         if storage_metadata.get('vaultlocker-encrypt', False):
             mountbase = storage_metadata.get('vaultlocker-mountbase')
-            vaultlocker.encrypt_storage(storage_name, mountbase)
+            layer.vaultlocker.encrypt_storage(storage_name, mountbase)
     set_flag('layer.vaultlocker.ready')
